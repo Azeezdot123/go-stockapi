1638b920f72b14a9cd876ce1cfa58b5d977a23db
azeezdot123 <headofstate123@gmail.com>
2022-08-15 22:35:18 +0100

    create main.go which is the entry point to project

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/go-stockapi/router"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Print("Starting Server on port 8000.....")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
