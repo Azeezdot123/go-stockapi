8e1846a6dfa7bfd98d95ad6ecae26cc7014e7767
azeezdot123 <headofstate123@gmail.com>
2022-08-15 22:26:05 +0100

    create the routes endpoint

--- router/router.go
+++ router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/go-stockapi/middleware"
+)
+func Router() *mux.Router{
+	router := mux.NewRouter()
+	
+	router.Handlefunc("/api/stock", middleware.GetAllStock).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
+	router.Handlefunc("/api/newstock", middleware.CreateStock).Methods("POST", "OPTIONS")
+	router.Handlefunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/stock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+}
