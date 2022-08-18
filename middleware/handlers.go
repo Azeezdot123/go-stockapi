3ce77c04625b051dae5724f37beb1189b145fca7
azeezdot123 <headofstate123@gmail.com>
2022-08-18 15:44:52 +0100

    create the handlers

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,1 +1,17 @@
-package handlers
+package middleware
+
+import "database/sql"
+
+type response struct{
+	ID			int64	`json:"id,omitempty"`
+	Message		string	`json:"message,omitempty"` 
+}
+
+func CreateConnection() *sql.Db{
+	err := getdotenv.Load(".env")
+}
+func GetStock(){}
+func GetAllStock(){}
+func CreateStock(){}
+func UpdateStock(){}
+func DeleteStock(){}
