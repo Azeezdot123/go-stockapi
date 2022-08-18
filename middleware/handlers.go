c391adc7a2c5b1931be42a65b693fdad6bb16bc3
Olamilekan <headofstate123@gmail.com>
2022-08-18 15:21:08 +0000

    create a connection to db

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,17 +1,33 @@
 package middleware
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
 
-type response struct{
-	ID			int64	`json:"id,omitempty"`
-	Message		string	`json:"message,omitempty"` 
+type response struct {
+	ID      int64  `json:"id,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
-func CreateConnection() *sql.Db{
-	err := getdotenv.Load(".env")
+func CreateConnection() *sql.DB {
+	err := gotdotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Succesfully Connected to Database")
+	return db
 }
-func GetStock(){}
-func GetAllStock(){}
-func CreateStock(){}
-func UpdateStock(){}
-func DeleteStock(){}
